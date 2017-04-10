94786853ba859a6964751c3c733df899b2d9f51b
Terfa Tortema <terfa.tortema@gmail.com>
2017-04-10 18:44:51 +0100

    merging the fizzbuzz branch to the master branch

---
+++ fizzbuzz/spec/tests.js
@@ -0,0 +1,40 @@
+// This is a test suite that with test cases for the fizz buzz tasks
+describe("Fizz Buzz tests ", function() {
+  it("should return `Fizz` for number divisible by 3", function() {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  });
+  it("should return `Buzz` for number divisible by 5", function() {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it("should return `FizzBuzz` for 15", function() {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 45", function() {
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 90", function() {
+    expect(fizzBuzz(90)).toBe('FizzBuzz');
+  });
+
+  it("should return `Fizz` for 63", function() {
+    expect(fizzBuzz(63)).toBe('Fizz');
+  });
+  it("should return `Buzz` for 100", function() {
+    expect(fizzBuzz(100)).toBe('Buzz');
+  });
+
+  it("should return 7 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(7)).toBe(7);
+  });
+
+  it("should return 101 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(101)).toBe(101);
+  });
+
+  it("should return `Undefined` if the function is undefined", function() {
+    expect(fizzBuzz()).toBeUndefined('The function is undefined');
+  });
+});
---
+++ fizzbuzz/src/app.js
@@ -0,0 +1,15 @@
+//This is the fizz buzz function that is be tested using jasmine
+function fizzBuzz(n){
+	if(n % 3 === 0 && n % 5 === 0){
+		return 'FizzBuzz';
+	}
+	else if(n % 3===0){
+		return 'Fizz';
+	}
+	else if (n % 5 === 0){
+		return 'Buzz';
+	}
+	else{
+		return n
+	}
+}
